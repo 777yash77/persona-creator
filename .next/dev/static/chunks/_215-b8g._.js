952d0fbd1318397df70fb04328a35b3b4f9639fe
Yash <ywanth224@gmail.com>
2026-09-24 12:23:51 +0530

    chore: ignore .next directory and cleanup tracked files

--- .next/dev/static/chunks/_215-b8g._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_1bmlcgb._.css","static/chunks/node_modules_1_3v30j._.js","static/chunks/src_0gu_qez._.js","static/chunks/_0w0gt1u._.js","static/chunks/src_views_CreateWizard_1yna6-h.css"],
-    source: "entry"
-});
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_1bmlcgb._.css","static/chunks/node_modules_1_3v30j._.js","static/chunks/src_0gu_qez._.js","static/chunks/_148dhaf._.js","static/chunks/src_views_Dashboard_1h397k9.css"],
-    source: "entry"
-});
